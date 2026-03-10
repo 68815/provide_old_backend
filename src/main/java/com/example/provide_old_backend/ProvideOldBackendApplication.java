c1e18a92773ee4cf29f435c9dd0da13d58dd97ac
0109L_ <Axty0903@outlook.com>
2026-03-10 16:01:19 +0800

    init commit feel..

---
+++ src/main/java/com/example/provide_old_backend/ProvideOldBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.provide_old_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvideOldBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProvideOldBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/provide_old_backend/ProvideOldBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.provide_old_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvideOldBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
