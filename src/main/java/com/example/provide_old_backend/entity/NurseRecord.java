8e66ca47abe9d2d890ca3cdc9c7f22a81b23ee0c
0109L_ <Axty0903@outlook.com>
2026-03-17 17:03:03 +0800

    fix:dateTime format

--- src/main/java/com/example/provide_old_backend/entity/NurseRecord.java
+++ src/main/java/com/example/provide_old_backend/entity/NurseRecord.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -26,6 +27,7 @@ public class NurseRecord implements Serializable {
     private Integer itemId;
 
     @TableField("nursing_time")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private LocalDateTime nursingTime;
 
     @TableField("nursing_content")
